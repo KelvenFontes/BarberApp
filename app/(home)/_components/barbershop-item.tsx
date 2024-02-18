ae76ce4f0c4d55812e40e902aced5bc9fd7b4087
Kelven Fontes <kelvenbentofontes@gmail.com>
2024-02-18 20:54:39 -0300

    feat: add Evaluation

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,6 +1,8 @@
+import { Badge } from "@/app/components/ui/badge";
 import { Button } from "@/app/components/ui/button";
 import { Card, CardContent } from "@/app/components/ui/card";
 import { Barbershop } from "@prisma/client";
+import { StarIcon } from "lucide-react";
 import Image from "next/image";
 
 interface BarbershopItemProps {
@@ -14,7 +16,14 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
       <CardContent className="p-0 pt-1">
 
         <div className="relative h-[160px] w-full">
-          <Image src={barbershop.imageUrl} height={0} width={0} sizes="100W" className="px-1 rounded-2xl" fill style={{objectFit: 'cover'}} alt={barbershop.name} />
+          <div className="absolute top-1 left-2 z-50">
+          <Badge variant="secondary" className="opacity-90 flex gap-1 items-center">
+            <StarIcon size={12} className="fill-primary text-primary"/>
+            <span className="text-xs">5,0</span>
+          </Badge>
+          </div>
+
+          <Image src={barbershop.imageUrl} height={0} width={0} sizes="100W" className="px-1 rounded-2xl" fill style={{ objectFit: 'cover' }} alt={barbershop.name} />
         </div>
 
         <div className="px-3 pb-3">
