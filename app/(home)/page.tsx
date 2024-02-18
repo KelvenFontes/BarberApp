7430559fbaf98b7258bbd823f95dee415a473b51
Kelven Fontes <kelvenbentofontes@gmail.com>
2024-02-18 17:57:20 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
+import { format } from "date-fns";
 import Header from "../components/header";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Hello, Kelven!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' MMMM do'")}
+        </p>
+      </div>
     </div>
   );
 }
