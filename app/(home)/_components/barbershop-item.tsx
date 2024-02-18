c723bcc282ac428f3707e6f3ea23260305653d43
Kelven Fontes <kelvenbentofontes@gmail.com>
2024-02-18 20:45:46 -0300

    feat: add barbershop recommended

---
+++ app/(home)/_components/barbershop-item.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/app/components/ui/button";
+import { Card, CardContent } from "@/app/components/ui/card";
+import { Barbershop } from "@prisma/client";
+import Image from "next/image";
+
+interface BarbershopItemProps {
+  barbershop: Barbershop;
+}
+
+const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+
+  return (
+    <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
+      <CardContent className="p-0 pt-1">
+
+        <div className="relative h-[160px] w-full">
+          <Image src={barbershop.imageUrl} height={0} width={0} sizes="100W" className="px-1 rounded-2xl" fill style={{objectFit: 'cover'}} alt={barbershop.name} />
+        </div>
+
+        <div className="px-3 pb-3">
+          <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">{barbershop.name}</h2>
+          <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barbershop.address}</p>
+
+          <Button className="w-full mt-3" variant="secondary">Reserve</Button>
+        </div>
+
+      </CardContent>
+    </Card>
+  );
+}
+
+export default BarbershopItem;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,8 +2,14 @@ import { format } from "date-fns";
 import Header from "../components/header";
 import Search from "./_components/search";
 import BookingItem from "../components/booking-item";
+import { db } from "../lib/prisma";
+import BarbershopItem from "./_components/barbershop-item";
 
-export default function Home() {
+export default async function Home() {
+
+  // Chamar prima e pegar barbearia
+  const barbershops = await db.barbershop.findMany({})
+
   return (
     <div>
       <Header />
@@ -22,6 +28,16 @@ export default function Home() {
       <div className="px-5 mt-6">
         <h2 className="mb-3 text-xs uppercase text-gray-400 font-bold">Appointments</h2>
         <BookingItem />
+      </div>
+
+      <div className="mt-6">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recommended</h2>
+
+        <div className="px-5 flex gap-4 overflow-x-auto [&::webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
 
     </div>
